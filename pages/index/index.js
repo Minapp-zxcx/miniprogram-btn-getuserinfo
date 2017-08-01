d2c38f4f1fc792c4c432ace122400c75f8370980
Astrian Zheng <mingriqd@gmail.com>
2017-08-01 10:08:55 +0800

    add bindtap event on btn

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,5 +11,16 @@ Page({
         userInfo: res
       })
     }, userInfoRaw)
+  },
+  canILogin(){
+    console.log(wx.canIUse('button.open-type.getUserInfo'))
+    if(!wx.canIUse('button.open-type.getUserInfo')){
+      wx.showModal({
+        title: "微信版本太旧",
+        content: '使用旧版本微信，将无法登录、使用一些功能。请至 App Store、Play Store 或其他可信渠道更新微信。',
+        confirmText:'好',
+        showCancel: false
+      })
+    }
   }
 })
